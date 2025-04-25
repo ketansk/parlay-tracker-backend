0984d803df9752e574608ae50d0f3cfc0c66af87
kulkarks <ketan.s.kulkarni56@gmail.com>
2025-04-25 18:09:31 -0400

    Add in directory

---
+++ app/models/parlay_models.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel
+from typing import List
+
+
+class ParlayLeg(BaseModel):
+    match: str
+    bet: str
+    result: str = ""
+
+
+class Parlay(BaseModel):
+    wager: float
+    odds: float
+    legs: List[ParlayLeg]
+    status: str = "pending"
